923e30a30185d5555def893ada0e8764257984b2
TMTanmay1 <tanmaymeshram883@gmail.com>
2024-03-31 03:35:08 +0530

    ver_nav fixed

--- Swalook-master/src/components/Pages/VertNav.js
+++ Swalook-master/src/components/Pages/VertNav.js
@@ -11,26 +11,26 @@ function VertNav() {
     <div className='vert_nav_main_c'>
         <div className="icon-container">
             <Link to="/appointment">
-              <BookOnlineIcon style={{ fontSize: 30, margin: '10px', color: 'white' }}/>
+              <BookOnlineIcon style={{ fontSize: 27, margin: '5px', color: 'white' }}/>
               <span className="icon-text">Appointment</span>
             </Link>
           </div>
           <div className="icon-container">
             <Link to="/generatebill">
-              <DescriptionIcon style={{ fontSize: 30, margin: '10px', color: 'white' }} />
+              <DescriptionIcon style={{ fontSize: 27, margin: '5px', color: 'white' }} />
               <span className="icon-text">Invoice</span>
             </Link>
           </div>
           <div className="icon-container">
-            <ShowChartIcon style={{ fontSize: 30, margin: '10px', color: 'white' }} />
+            <ShowChartIcon style={{ fontSize: 27, margin: '5px', color: 'white' }} />
             <span className="icon-text">Analysis</span>
           </div>
           <div className="icon-container">
-            <StorefrontIcon style={{ fontSize: 30, margin: '10px', color: 'white' }} />
+            <StorefrontIcon style={{ fontSize: 27, margin: '5px', color: 'white' }} />
             <span className="icon-text">Store</span>
           </div>
           <div className='icon-container'>
-            <p style={{ fontSize: 15, fontWeight:'bold' ,margin: '10px', color: 'white' }}>CLP</p>
+            <p style={{ fontSize: 15, fontWeight:'bold' ,margin: '5px',  marginTop:'5px',color: 'white' }}>CLP</p>
             <span className="icon-text">Customer Loyality Program</span>
           </div>
     </div>
